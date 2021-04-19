25ae135c957f020780b522cca0827488754653d1
Gerardo Gallegos <puxerop@gmail.com>
2021-04-19 08:09:47 -0500

    Pipe for validate IDs of MongoDB

---
+++ src/common/pipes/mongo-id.pipe.ts
@@ -0,0 +1,12 @@
+import { Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
+import { isMongoId } from 'class-validator';
+
+@Injectable()
+export class MongoIdPipe implements PipeTransform {
+  transform(value: string) {
+    if (!isMongoId(value)) {
+      throw new BadRequestException(`${value} is not a mongoId`);
+    }
+    return value;
+  }
+}

8b3f4a53939dbc0487b814b49a54404ec5b6d792
Gerardo Gallegos <puxerop@gmail.com>
2021-04-26 07:19:55 -0500

    Adding CRUD operations for Products

--- src/products/controllers/products/products.controller.ts
+++ src/products/controllers/products/products.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  HttpCode,
+  HttpStatus,
+} from '@nestjs/common';
+import { ProductsService } from 'src/products/services/products/products.service';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
+import { MongoIdPipe } from 'src/common/pipes/mongo-id.pipe';
+import {
+  CreateProductDto,
+  FilterProductDto,
+  UpdateProductDto,
+} from 'src/products/dtos/products.dto';
 
+@ApiTags('Products')
 @Controller('products')
-export class ProductsController {}
+export class ProductsController {
+  constructor(private productService: ProductsService) {}
+
+  @Get()
+  @ApiOperation({ summary: 'List of products' })
+  @HttpCode(HttpStatus.ACCEPTED)
+  getAll(@Query() params?: FilterProductDto) {
+    return this.productService.getAll(params);
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Create new product' })
+  create(@Body('product') product: CreateProductDto) {
+    return this.productService.create(product);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get single product by id' })
+  getById(@Param('id', MongoIdPipe) id: string) {
+    return this.productService.getById(id);
+  }
+
+  @Put(':id')
+  @ApiOperation({ summary: 'Update single product by id' })
+  update(
+    @Param('id', MongoIdPipe) id: string,
+    @Body('product') product: UpdateProductDto,
+  ) {
+    return this.productService.update(id, product);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete single product by id' })
+  delete(@Param('id', MongoIdPipe) id: string) {
+    return this.productService.delete(id);
+  }
+}
--- src/products/services/products/products.service.ts
+++ src/products/services/products/products.service.ts
@@ -1,4 +1,89 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from 'src/products/entities/product.entity';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import {
+  CreateProductDto,
+  UpdateProductDto,
+  FilterProductDto,
+} from 'src/products/dtos/products.dto';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<Product>,
+  ) {}
+
+  async getAll(params?: FilterProductDto) {
+    const { limit = 20, offset = 0 } = params;
+
+    const [total, users] = await Promise.all([
+      this.productModel.countDocuments(),
+      this.productModel
+        .find()
+        .skip(offset * limit)
+        .limit(limit)
+        .exec(),
+    ]);
+
+    return {
+      limit,
+      offset,
+      total,
+      users,
+    };
+  }
+
+  async getById(id: string) {
+    const user = await this.productModel.findById(id);
+
+    if (!user) {
+      throw new NotFoundException(`User not found: ${id}`);
+    }
+
+    return {
+      user,
+    };
+  }
+
+  async create(product: CreateProductDto) {
+    const newProduct = await this.productModel.create({
+      ...product,
+      slug: product.title.replace(' ', '-'),
+      seller: '618c75d5895df75d10500966',
+    });
+    return newProduct;
+  }
+
+  async update(id: string, product: UpdateProductDto) {
+    let productData = await this.productModel.findById(id);
+
+    if (!productData) {
+      throw new NotFoundException('User not exist');
+    }
+
+    productData = await this.productModel.findOneAndUpdate(
+      { _id: id },
+      product,
+      {
+        new: true,
+      },
+    );
+
+    return productData;
+  }
+
+  async delete(id: string) {
+    const data = await this.productModel.findById(id);
+
+    if (!data) {
+      throw new NotFoundException('Product not found');
+    }
+
+    await data.remove();
+
+    return {
+      message: `User was deleted: ${id}`,
+    };
+  }
+}

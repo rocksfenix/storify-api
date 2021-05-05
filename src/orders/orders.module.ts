c41817c8a1547e99d03114a258629c942ff11e09
Gerardo Gallegos <puxerop@gmail.com>
2021-05-05 08:23:03 -0500

    Adding Order schema in orders module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { OrdersController } from './controllers/orders.controller';
 import { OrdersService } from './services/orders.service';
+import { Order, OrderSchema } from './entities/order.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Order.name,
+        schema: OrderSchema,
+      },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService]
 })
